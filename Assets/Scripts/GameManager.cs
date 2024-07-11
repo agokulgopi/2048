37315020970e96882d5895d3b137b2a8d1b0855e
Gokul Gopi <102305352+agokulgopi@users.noreply.github.com>
2024-07-11 11:41:58 +0530

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TileBoard board;
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    public void NewGame()
+    {
+        board.ClearBoard();
+        board.CreateTile();
+        board.CreateTile();
+        board.enabled = true;
+    }
+
+    public void GameOver()
+    {
+        board.enabled = false;
+    }
+}
